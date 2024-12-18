e179b3c2a75b7bf0f2b47b397ca0a70bfa5661c0
Nissyaniss <kirito042004@gmail.com>
2024-12-18 23:11:28 +0100

    partially fixed history but need more work

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::{
+	fs::{self, File},
 	io::{self, stdout, Write},
 	ops::Index,
 	process::Command,
@@ -53,6 +54,11 @@ fn print_flush(string: &str) {
 	let _ = stdout().flush();
 }
 
+fn clear_prompt() {
+	print!("{}", " ".repeat(15)); //BAD BUT DONT KNOW FOR NOW SO TODO
+	let _ = stdout().flush();
+}
+
 #[allow(clippy::option_if_let_else)]
 fn handle_command(input: &str) -> i32 {
 	let mut input = input.split_whitespace();
@@ -74,26 +80,39 @@ fn handle_command(input: &str) -> i32 {
 fn print_events() -> io::Result<()> {
 	let prompt = String::from("> ");
 	let mut command = String::new();
-	let mut history = Vec::new();
-	let mut history_index = 0;
+	let history_data = fs::read_to_string("shitshell_history").unwrap_or_else(|_| {
+		let _ = File::create("./shitshell_history"); //Bad
+		String::new()
+	});
+	let mut history: Vec<String> = history_data
+		.split('\n')
+		.map(std::string::ToString::to_string)
+		.collect();
+	let mut history_index: usize = history.len() - 1;
 	print_flush(&prompt);
 	loop {
 		let event = crossterm::event::read()?;
 		match event {
 			Event::Key(event) if event.kind == KeyEventKind::Press => {
+				//Bad code but will do for now
+				//CTRL+d to quit
 				if event.code == Char('d') && event.modifiers == KeyModifiers::CONTROL {
 					break;
+				//Enter to validate
 				} else if event.code == Enter {
 					let result = handle_command(&command);
 					history.push(command);
+					history_index = history.len() - 1;
 					command = String::new();
 					if result == 0 {
 						print_flush(&format!("\n\r{prompt}"));
 					} else {
 						print_flush(&format!("\r{prompt}"));
 					}
+				//CTRL+c to cancel (not done)
 				} else if event.code == Char('c') && event.modifiers == KeyModifiers::CONTROL {
 					print_flush(&format!("\n\r{prompt}"));
+				//Backspace
 				} else if event.code == Backspace {
 					if command.is_empty() {
 						print_flush(&format!("\r{prompt}"));
@@ -101,22 +120,28 @@ fn print_events() -> io::Result<()> {
 						print_flush(&format!("\r{prompt}{command}\x08 \x08"));
 					}
 					command.pop();
+				//Space
 				} else if event.code == Char(' ') {
 					command.push(' ');
 					print_flush(&format!("\r{prompt}{command}"));
+				//Characters
 				} else if event.is_a_character() && event.modifiers == KeyModifiers::empty() {
 					command.push(event.get_char().unwrap());
 					print_flush(&format!("{}", event.code));
+				//History up
 				} else if event.code == Up {
-					history_index += 1;
-					if history.len() < history_index {
-						command = history.index(history.len() - history_index).to_string();
+					history_index = history_index.saturating_sub(1);
+					if history.len() > history_index {
+						command = history.index(history_index).to_string();
+						clear_prompt();
 						print_flush(&format!("\r{prompt}{command}"));
 					}
+				//History Down
 				} else if event.code == Down {
-					history_index -= 1;
-					if history.len() > history_index {
-						command = history.index(history.len() - history_index).to_string();
+					history_index += 1;
+					if history_index < history.len() {
+						command = history.index(history_index).to_string();
+						clear_prompt();
 						print_flush(&format!("\r{prompt}{command}"));
 					}
 				}
